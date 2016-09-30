5ea6375949198dee971ca2220ffd1e60435dbd3f
Rashmi2595 <nirmalrashmi@gmail.com>
2016-09-30 17:50:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/myarticleone', function (req, res) {
+
+app.get('/article-one', function (req, res) {
   res.sendFile("Article 1 requessted and serverd");
 });
-app.get('/myarticletwo', function (req, res) {
+
+app.get('/article-two', function (req, res) {
   res.sendFile("Article 2 requessted and serverd");
 });
-app.get('/myarticlethree', function (req, res) {
+
+app.get('/article-three', function (req, res) {
   res.sendFile("Article 3 requessted and serverd");
 });
 app.get('/ui/madi.png', function (req, res) {
